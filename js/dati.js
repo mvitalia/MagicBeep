8accf7629a7318b20a911f5ff4d4261fc9e92e10
mvitalia <p.guglielmo@mvitalia.com>
2016-12-16 13:42:27 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -254,6 +254,7 @@ function successoSelectNotifica(tx,dati)
            li_dati+="<hr>";*/
         
           // $("#box_img_notifica").append(immagineNot);
+           $(".appendDettaglioNotifica").html("");
            $(".appendDettaglioNotifica").append(li_dati);
         }
        // Permette di "appendere" il codice html creato in dinamico con i dati
