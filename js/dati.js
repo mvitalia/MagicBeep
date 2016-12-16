287941c89db86516110f78af2ceac1c2cd835ef9
mvitalia <p.guglielmo@mvitalia.com>
2016-12-16 12:40:02 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -376,7 +376,7 @@ function apriNotifica(id)
     var prova = id;
      localStorage.setItem('Id_notifica', prova);
     $('.'+id+'').hide();
-    $( ":mobile-pagecontainer" ).pagecontainer( "change", "index.html/#lostPassword", {    transition: "flip", reload:true } );
+    $( ":mobile-pagecontainer" ).pagecontainer( "change", "index.html#lostPassword", {    transition: "flip", reload:true } );
 }
 
 function salvaNotifica(id)
