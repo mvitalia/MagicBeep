ed6bc2b17c49cac942d2710603e78ccce4d6507e
mvitalia <p.guglielmo@mvitalia.com>
2016-12-19 18:02:31 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -496,18 +496,22 @@ function caricaNotificheFiltrate () {
 
 function dinamico ()
     {
-       $.getJSON("http://magicbeep.mvclienti.com/webservices/slider_home.aspx", function (dati) {
+          alert("ok");
+      $.getJSON("http://magicbeep.mvclienti.com/webservices/slider_home.aspx", function (dati) {
                     var li_dati = "";
                     $.each(dati, function (i, name) {
                         // Inserisco dati nel db sqllite dell' App
-	
-                    li_dati+="<div class='swiper-slide'> <div class='slider-bottom-right valign-wrapper'><div class='valign center-align width-100 p-b-5em'>";
+	                 alert("ok");
+                    li_dati="<div class='swiper-slide'> <div class='slider-bottom-right valign-wrapper'><div class='valign center-align width-100 p-b-5em'>";
                     li_dati+="<h2 class='uppercase'>"+name.titolo+"</h2> <p>"+name.testo+"</p></div></div></div>";
                     });
 					 
                                 
                 });
-     
-        $(".swiper-wrapper").append(li_dati);
+     /* var dai="<div class='swiper-slide'> <div class='slider-bottom-right valign-wrapper'><div class='valign center-align width-100 p-b-5em'>";
+      dai+="<h2 class='uppercase'>Halo</h2> <p>Is a mobile website, developed to make your life easer!</p></div></div></div>";
+      dai+="<div class='swiper-slide'> <div class='slider-bottom-right valign-wrapper'><div class='valign center-align width-100 p-b-5em'>";
+      dai+="<h2 class='uppercase'>Halo</h2> <p>Is a mobile website, developed to make your life easer!</p></div></div></div>";*/
+        $(".swiper-wrapper").append(dai);
     }
  
