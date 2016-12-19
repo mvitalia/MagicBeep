1a13491ce53945a4039d38b6b050255590c577ef
mvitalia <p.guglielmo@mvitalia.com>
2016-12-19 11:10:25 +0100

    a

--- js/functions.js
+++ js/functions.js
@@ -63,6 +63,12 @@ $('#open-right-cinque').sideNav({
     closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
 });
 
+$('#open-right-sei').sideNav({
+    menuWidth: 240, // Default is 240
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
+});
+
 // Left Sidebar
 $('#open-left').sideNav({
     menuWidth: 240, // Default is 240
