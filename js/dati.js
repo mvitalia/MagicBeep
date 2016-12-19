7f7b06f85a69e3b8202d689c8a26a151ed38fb6f
mvitalia <p.guglielmo@mvitalia.com>
2016-12-19 17:54:51 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -493,4 +493,21 @@ function caricaNotificheFiltrate () {
                                          });
     });
 }
- //SELECT C.ID as ID_notifica,  N.*, C.* FROM notizie as N, notifiche as C WHERE C.ID_notizia=N.ID
+
+function dinamico ()
+    {
+       $.getJSON("http://magicbeep.mvclienti.com/webservices/slider_home.aspx", function (dati) {
+                    var li_dati = "";
+                    $.each(dati, function (i, name) {
+                        // Inserisco dati nel db sqllite dell' App
+	
+                    li_dati+="<div class='swiper-slide'> <div class='slider-bottom-right valign-wrapper'><div class='valign center-align width-100 p-b-5em'>";
+                    li_dati+="<h2 class='uppercase'>"+name.titolo+"</h2> <p>"+name.testo+"</p></div></div></div>";
+                    });
+					 
+                                
+                });
+     
+        $(".swiper-wrapper").append(li_dati);
+    }
+ 
